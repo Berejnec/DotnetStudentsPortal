493fad7e8f8699f4a2ae8a12661226a08f66935f
Adrian Berejnec <adrianberejnec29@gmail.com>
2024-04-07 02:22:53 +0300

    Setup Entity Framework and add initial Migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using DotnetStudentsPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetStudentsPortal.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
