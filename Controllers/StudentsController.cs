e8ad0d8548ea35c7187bc8093490b490379fb3a7
Adrian Berejnec <adrianberejnec29@gmail.com>
2024-04-07 15:15:41 +0300

    Add students list, Edit and Delete student operations

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using DotnetStudentsPortal.Models;
 using DotnetStudentsPortal.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotnetStudentsPortal.Controllers
 {
@@ -35,6 +36,54 @@ namespace DotnetStudentsPortal.Controllers
             await dbContext.SaveChangesAsync();
 
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+
+            return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+       
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students.AsNoTracking().FirstOrDefaultAsync(x => x.Id == viewModel.Id);
+
+            if (student is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
         }
     }
 }
